194a822d64eaa9bfd710d06fdc47ea90148c41e3
John Lapeyre <jlapeyre@users.noreply.github.com>
2023-01-28 00:44:50 -0500

    Remove commented out cruft in OQ code

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -79,35 +79,23 @@ out = measure q;
 def test_readme_circuit_physical_qubits():
     source = """
 OPENQASM 3.0;
-// The 'stdgates.inc' is supported, and the gates are only available if it
-// has correctly been included.
 include "stdgates.inc";
 
-// Parametrised inputs are supported.
 input float[64] a;
 
-// qubit[3] q;
 bit[2] mid;
 bit[3] out;
 
-// Aliasing and re-aliasing are supported.
-// let aliased = q[0:1];
-
-// Parametrised gates that make use of the stdlib.
 gate my_gate(a) c, t {
   gphase(a / 2);
   ry(a) c;
   cx c, t;
 }
 
-// Gate modifiers work as well; this gate is equivalent to `p(-a) c;`.
 gate my_phase(a) c {
   ctrl @ inv @ gphase(a) c;
 }
 
-// We handle mathematical expressions on gate creation and complex indexing
-// of temporary collections.
-//my_gate(a * 2) aliased[0], q[{1, 2}][0];
 my_gate(a * 2) $0, $1;
 measure $0 -> mid[0];
 measure $1 -> mid[1];
@@ -116,22 +104,11 @@ while (mid == "00") {
   reset $0;
   reset $1;
   my_gate(a) $0, $1;
-  // We support the builtin mathematical symbols.
   my_phase(a - pi/2) $1;
   mid[0] = measure $0;
   mid[1] = measure $1;
 }
 
-// The condition resolver can also handle simple cases that don't look
-// _exactly_ like equality conditions.
-//if (mid[0]) {
-// There is limited support for aliasing within nested scopes.
-//  let inner_alias = q[{0, 1}];
-//  reset inner_alias;
-//}
-
-// TODO: measure all ?
-//out = measure q;
 out[0] = measure $0;
 out[1] = measure $1;
 out[2] = measure $2;
