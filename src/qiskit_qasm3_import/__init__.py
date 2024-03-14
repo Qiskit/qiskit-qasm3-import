3afada35f1a70a73f29ec4de7821f53fe5ff78ee
Jake Lishman <jake.lishman@ibm.com>
2024-03-14 16:47:50 +0000

    Add licence headers (#29)
    This matches other Qiskit projects.  They originally weren't in these
    files because the project wasn't originally going to be managed by the
    Qiskit organisation, but was just a proof of concept.

--- src/qiskit_qasm3_import/__init__.py
+++ src/qiskit_qasm3_import/__init__.py
@@ -1,3 +1,14 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may obtain a copy of this license
+# in the LICENSE.txt file in the root directory of this source tree or at
+# http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this copyright notice, and modified
+# files need to carry a notice indicating that they have been altered from the originals.
+
 """Basic importer for OpenQASM 3 programmes into Qiskit."""
 
 __version__ = "0.4.1"
